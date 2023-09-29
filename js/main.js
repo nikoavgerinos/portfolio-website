d9344ac6a8fa3ea56da076a9a8c3045a19f5536a
nikoavgerinos <avgerinos.niko@gmail.com>
2023-09-29 15:00:02 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+<style>
+  .github-work-button {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #0074E4;
+    color: #fff;
+    text-decoration: none; /* Removes the underline on the link */
+    border-radius: 4px;
+  }
+</style>
